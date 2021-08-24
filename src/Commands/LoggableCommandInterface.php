a448fa2771c9e2eff3f6e61b04881cbcfada4e52
Christian Ramelow <info@codekandis.net>
2021-08-25 01:41:53 +0200

    Adds a loggable command interface.

---
+++ src/Commands/LoggableCommandInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Console\Commands;
+
+/**
+ * Represents the interface of any class able to log messages.
+ * @package codekandis/console
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface LoggableCommandInterface
+{
+}
