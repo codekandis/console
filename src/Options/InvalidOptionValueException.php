1e3db586422e5d698735493436d3760e1352f305
Christian Ramelow <info@codekandis.net>
2022-10-07 10:26:47 +0200

    Adds an invalid option value exception.

---
+++ src/Options/InvalidOptionValueException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Console\Options;
+
+use RuntimeException;
+
+/**
+ * Represents an exception if an option value is invalid.
+ * @package codekandis/console
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class InvalidOptionValueException extends RuntimeException
+{
+}
